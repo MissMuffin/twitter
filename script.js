b66587e3f2ddd99afcbbf6087ab9a2f36a1f252f
Kaj-Sören <kajmossi@yahoo.de>
2013-02-18 14:20:18 +0100

    Fina commit before presentation

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(document).ready(function($) {
-			$("#tweets").twitterTimeline("@ehlersmax");
-		});		
+	$('#keywords').bind('submit', function(event){
+		event.preventDefault();
+		var keyword = $('#keySearch').val();
+		$('#tweets').twitterSearch(keyword);
+	});
+
+	$("#userName").submit(function(){
+		var userSearchText = $("#userSearch").val();
+		$("#tweets").twitterTimeline("@"+userSearchText);
+		return false;
+	});
+});		
