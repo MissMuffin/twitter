0229a200c0a75f58e3deefe74a4c4f8ac3a227e1
Kaj-Sören <kajmossi@yahoo.de>
2013-02-18 14:27:55 +0100

    Or not?

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ $(document).ready(function($) {
 	$('#keywords').bind('submit', function(event){
 		event.preventDefault();
 		var keyword = $('#keySearch').val();
+		$('#tweets').html('');
 		$('#tweets').twitterSearch(keyword);
 	});
 
 	$("#userName").submit(function(){
 		var userSearchText = $("#userSearch").val();
+		$('#tweets').html('');
 		$("#tweets").twitterTimeline("@"+userSearchText);
 		return false;
 	});
