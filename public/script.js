d0a17af592011d8772fae1f3bd081a51ec96c1b6
Duana Saskia <starkcoffee+apps@gmail.com>
2013-03-02 22:05:03 +0100

    add service to get users from server

--- public/script.js
+++ public/script.js
@@ -15,10 +15,8 @@ $(document).ready(function($) {
 
 
   if (window.location.hostname == "localhost" || window.location.hostname == ""){
-	  
-	  $('#tweets').twitterSearch("bghackathon");
-
+	  $('#tweets').twitterSearch("#bghackathon");
   } else {
-	  $('#keySearch').val(window.location.pathname);
+	  $('#keySearch').val('#' + window.location.pathname);
   }
 });
--- server.rb
+++ server.rb
@@ -1,5 +1,8 @@
 require 'rubygems'
 require 'sinatra'
+require 'httparty'
+require 'json'
+require 'open-uri'
 
 set :public_folder, 'public'
 
@@ -7,3 +10,18 @@ get "/:hashtag" do
   File.read('public/index.html')
 end
 
+get "/users/:hashtag" do
+  keyword = URI::encode(params[:hashtag])
+  response = HTTParty.get("http://search.twitter.com/search.json?q=#{keyword}")
+  users = response["results"].map{|t| to_user(t)}
+  users = users.uniq{|u| u[:id]}
+  users.to_json
+end
+
+private
+def to_user(tweet_hash)
+  { :username=> tweet_hash["from_user_name"],
+    :profile_image_url => tweet_hash["profile_image_url"],
+    :id => tweet_hash["from_user_id"]
+  }
+end
