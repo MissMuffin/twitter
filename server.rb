d925417bb0cf6dd8a035f37ff7dc97c3956ce04e
Duana Saskia <starkcoffee+apps@gmail.com>
2013-03-03 06:33:07 +0100

    store users is mongo

--- server.rb
+++ server.rb
@@ -18,26 +18,22 @@ def get_connection
   @db_connection
 end
 
-get "/mongo" do
-  @db = get_connection
-  @coll   = @db['test']
-
-  3.times do |i|
-      @coll.insert({'a' => i+1})
-  end
-  "There are #{@coll.count} records."
-end
-
-
 get "/:hashtag" do
   File.read('public/index.html')
 end
 
 get "/users/:hashtag" do
-  keyword = URI::encode("#" + params[:hashtag])
-  response = HTTParty.get("http://search.twitter.com/search.json?q=#{keyword}&rpp=100")
-  users = response["results"].map{|t| to_user(t)}
-  users = users.uniq{|u| u[:id]}
+  hashtag = params[:hashtag]
+  tweets = get_tweets(hashtag)
+
+  tweets.each do |t|
+    user = get_user(hashtag, t["from_user_id"])
+    if !user
+      store_user(hashtag, to_user(t))
+    end
+  end
+
+  users = get_all_users(hashtag)
 
   status 200
   headers({ "Content-Type" => "application/json",
@@ -47,9 +43,37 @@ get "/users/:hashtag" do
 end
 
 private
+
+def get_collection(hashtag)
+  @db = get_connection
+  coll = @db['users.' + hashtag]
+end
+
+def store_user(hashtag, user)
+  get_collection(hashtag).insert(user)
+end
+
+def get_user(hashtag, twitter_id)
+  get_collection(hashtag).find_one("twitter_id" => twitter_id)
+end
+
+def get_all_users(hashtag)
+  get_collection(hashtag).find.to_a
+end
+
+def get_tweets(hashtag)
+  keyword = URI::encode("#" + hashtag)
+  response = HTTParty.get("http://search.twitter.com/search.json?q=#{keyword}&rpp=100")
+  response["results"]
+end
+
 def to_user(tweet_hash)
   { :username=> tweet_hash["from_user_name"],
     :profile_image_url => tweet_hash["profile_image_url"],
-    :id => tweet_hash["from_user_id"]
+    :twitter_id => tweet_hash["from_user_id"],
+    :last_tweet => {
+      :text => tweet_hash["text"],
+      :twitter_id => tweet_hash["id"],
+    }
   }
 end
